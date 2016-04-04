e6bdcdcd35bb0188ee4d32bffe23b46e85a79813
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-04-04 13:43:01 -0700

    Designed a Puppy Class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#SPECIES ------------------------
+#Canis lupus familiaris
+
+#CHARACTERISTICS ----------------
+#Fur color: varies
+#Size: varies
+#Number of leg: 4
+#Name: varies
+#Athletic: yes
+
+#BEHAVIOR -----------------------
+#Bark
+#Run
+#Sleep
