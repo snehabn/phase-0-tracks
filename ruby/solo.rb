e0f8a2005875342a40474c0dd41c99998f96c555
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-04-06 16:50:59 -0700

    Designed my Camera Class

---
+++ ruby/solo.rb
@@ -0,0 +1,17 @@
+# CAMERA ------------------------
+# Nikon
+
+# CHARACTERISTICS ----------------
+# Body model: varies
+# Lens brand: Nikkor
+# Lens model: varies
+# cost: varies
+# Number owned: 2
+# Picture file format: RAW
+
+# BEHAVIOR -----------------------
+# Take Pictures
+# Take videos
+# Enhance photos
+# Make a movie
+# Make a Slide show
