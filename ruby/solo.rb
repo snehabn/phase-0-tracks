0ada529c1f52a193934b82e0482af054ea02868b
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-04-06 19:19:24 -0700

    Improved on the class design

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,17 +1,22 @@
 # CAMERA ------------------------
 # Nikon
 
-# CHARACTERISTICS ----------------
+# CHARACTERISTICS ----------------attributes
 # Body model: varies
 # Lens brand: Nikkor
 # Lens model: varies
 # cost: varies
 # Number owned: 2
 # Picture file format: RAW
+# Pictures: array of mountain, ocean, valley, city, lake
 
-# BEHAVIOR -----------------------
-# Take Pictures
-# Take videos
-# Enhance photos
-# Make a movie
+# BEHAVIOR -----------------------methods
+# Take Pictures(number)
+###return statement indicating number of pictures taken.
+# Enhance photos(Picture)
+### identify the Picture in the Pictures array
+### put that picture first in the array as recently modified.
+# Make a movie(hours)
+### convert hours to minutes
+### return the total minutes
 # Make a Slide show
