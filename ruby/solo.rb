8015c9c7ac7c05d50640773d5efb4e6f8ae27252
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-04-08 11:48:05 -0700

    Added in design of class, wrote code for class Camera. Started on Release 2 to write a program implementing the class.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -6,8 +6,7 @@
 # Body model: D7100, varies
 # Lens brand: Nikkor, varies
 # cost: varies
-# Number owned: 2
-# Picture file format: RAW
+# Picture file format: RAW, varies
 # Album: array of mountain, ocean, valley, city, lake
 
 # BEHAVIOR -----------------------methods
@@ -19,7 +18,8 @@
 # Make a movie(hours)
 ### convert hours to minutes
 ### return the total minutes
-# Make a Slide show
+# Find a photo
+### access and return the photo from the pictures array. Move it to the zeroth element of the array. print array
 
 class Camera
 	
@@ -31,11 +31,13 @@ class Camera
 		@body_model = body_model
 		@lens_brand = lens_brand
 		@cost = 0
-		@file_format = file_format
+		@file_format = "RAW"
+		puts "Initializing new camera instance..."
 	end
 	
 	def photo_album
 		@picture = ["mountain", "ocean", "valley", "city", "lake" ]
+		p @picture
 	end
 	
 	def take_picture(num_of_pic)
@@ -43,7 +45,8 @@ class Camera
 	end
 	
 	def add_to_album(picture)
-		
+		@picture << picture
+		p @picture
 	end
 	
 	def make_movie(hours)
@@ -51,8 +54,48 @@ class Camera
 		puts "Your movie is #{minutes} minutes long."
 	end
 	
-	def slideshow
+	def find_photo(photo)
+		@photo = photo
+		@picture.delete(@photo)
+		@picture.insert(0, @photo)
 		p @picture
 	end
-
 end
+
+nikon = Camera.new("Nikon", "D7100", "Nikkor")
+p nikon.photo_album
+p nikon.add_to_album("volcano")
+p nikon.cost = 1000
+p nikon.find_photo("volcano")
+p nikon
+
+#Release 2
+
+puts "Hello and welcome to your camera portal. What is your name?"
+name = gets.chomp.capitalize
+
+puts "Hello #{name}, what camera brand do you own?"
+camera_model = gets.chomp
+
+puts "Now, what model of the body do you have?"
+body_model = gets.chomp
+
+puts "Finally what lens are you using?"
+lens_brand = gets.chomp
+
+new_camera = Camera.new(camera_model, body_model, lens_brand)
+
+puts "Thank you for the information!"
+puts "Here is a menu of available actions:"
+puts " "
+puts "What would you like to do?"
+action = gets.chomp.downcase
+
+case action
+
+when action.include? "take"
+	puts "Please enter name of picture to add to your album."
+	
+
+	
+	
