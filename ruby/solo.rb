6a5f92afcf18491c7e1d8a39d456456577c2f9cd
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-04-08 19:20:19 -0700

    Inserted loops to ask user for info multiple times.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,3 +1,4 @@
+	
 # CAMERA ------------------------
 # Nikon
 
@@ -24,7 +25,7 @@
 class Camera
 	
 	attr_reader :camera_model, :body_model, :lens_brand
-	attr_accessor :cost, :file_format
+	attr_accessor :cost, :file_format, :picture
 	
 	def initialize(camera_model, body_model, lens_brand)
 		@camera_model = camera_model
@@ -32,12 +33,12 @@ class Camera
 		@lens_brand = lens_brand
 		@cost = 0
 		@file_format = "RAW"
+		@picture
 		puts "Initializing new camera instance..."
 	end
 	
 	def photo_album
 		@picture = ["mountain", "ocean", "valley", "city", "lake" ]
-		p @picture
 	end
 	
 	def take_picture(num_of_pic)
@@ -45,7 +46,7 @@ class Camera
 	end
 	
 	def add_to_album(picture)
-		@picture << picture
+		@picture.insert(0, picture)
 		p @picture
 	end
 	
@@ -56,7 +57,7 @@ class Camera
 	
 	def find_photo(photo)
 		@photo = photo
-		@picture.delete(@photo)
+		@picture.delete(photo)
 		@picture.insert(0, @photo)
 		p @picture
 	end
@@ -74,14 +75,23 @@ p nikon
 puts "Hello and welcome to your camera portal. What is your name?"
 name = gets.chomp.capitalize
 
-puts "Hello #{name}, what camera brand do you own?"
+puts "How many cameras would you like to enter?"
+num_of_cameras = gets.chomp.to_i
+
+num_of_cameras.times do
+	
+puts "Please enter camera brand:"
 camera_model = gets.chomp
 
-puts "Now, what model of the body do you have?"
+puts "Now, what model of the #{camera_model} do you have?"
 body_model = gets.chomp
 
-puts "Finally what lens are you using?"
+puts "What brand lens are you using?"
 lens_brand = gets.chomp
+
+puts "Finally, how much did your camera cost?"
+camera_cost = gets.chomp.to_i
+
 
 new_camera = Camera.new(camera_model, body_model, lens_brand)
 
@@ -92,14 +102,59 @@ puts "2 - Take a picture"
 puts "3 - Add photo to album"
 puts "4 - Make a movie"
 puts "5 - Find a photo from your album"
-puts "What would you like to do? Please enter a number corresponding to your choice."
-action = gets.chomp.to_i
-p action
-# case action
 
-# when action.include? "take"
-# 	puts "Please enter name of picture to add to your album."
-	
+choice = false
 
+until choice
+
+	puts "What would you like to do? Please enter a number corresponding to your choice."
+	action = gets.chomp.to_i
 	
+	case action
 	
+	when 1
+		puts "Here are the photos currently in your album:"
+		new_camera.photo_album.each do |photo|
+			puts "#{photo}"
+		end
+		choice = true
+	when 2
+		puts "How many photos would you like to take?"
+		photos_to_take = gets.chomp.to_i
+		puts "Get ready to take #{photos_to_take} photos!"
+		photos_to_take.times do |i|
+			puts "Say cheese!"
+			puts "Click!"
+		end
+		choice = true
+	when 3
+		puts "Please list the names of the photos you wish to add to your album:"
+		photo_to_album = gets.chomp
+		p new_camera.photo_album
+		new_camera.add_to_album(photo_to_album)
+		p new_camera.photo_album
+		# puts "Here is your updated album library:"
+		
+		# new_camera.photo_album.each do |photo|
+		# 	puts "#{photo}"
+		# end
+		choice = true
+	when 4
+		puts "How many hours do you want your movie to be?"
+		movie_duration = gets.chomp.to_f
+		movie_minutes = movie_duration * 60
+		p "Your movie will be #{movie_minutes} minutes long."
+		choice = true
+	when 5
+		puts "Please enter the name of the photo to find:"
+		locate_photo = gets.chomp.downcase
+		new_camera.find_photo(locate_photo)
+		choice = true
+	else
+		puts "You have entered invalid number. Please choose from choices 1 thru 5."
+		choice = false
+end
+end
+end
+
+puts "Thank you for visiting this camera portal!"
