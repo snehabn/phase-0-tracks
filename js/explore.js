1d922f349ee84e94bfd2714546b2c0acdc575a58
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-04-17 12:26:56 -0700

    Pseudocoded and wrote a function for reversing a string. Wrote a function that prints the result of the reverse function based on a conditional. Wrote driver code to test out the previously written functions.

---
+++ js/explore.js
@@ -0,0 +1,51 @@
+
+// Method: Take a string and return the reverse of it.
+
+// Input: String
+
+// Steps:
+/// take each character in the string and return in reverse order.
+//// starting at the end of the string, FOR each character, return the character and decrement by an index of 1 to the next character
+/// append the individual characters to an initialized array
+/// join the array of characters
+
+// Output:
+/// returns a string of the reverse of input.
+
+
+function reverse(str_input) {
+// var str_input = "Hello!";
+
+	var arr = [];
+	for (var i = str_input.length - 1; i >= 0; i--) {
+		arr.push(str_input[i]);		
+	}
+	var joined_output = arr.join("");
+	
+	return joined_output
+}
+
+// var hello = reverse("Hello");
+
+
+	// if (hello == "Hello"){
+	// 	console.log("Valid entry.")
+	// }
+	// else if (hello == "olleH"){
+	// 	console.log("Reversed entry.")
+	// }
+	// else {
+	// 	console.log("Not a valid entry.")
+	// }
+function cond_output(input) {
+	if (reverse(input).length <= 5) {
+		console.log("Your word has a length less or equal to 5.")
+		console.log(reverse(input));
+	}
+	else {
+		console.log("Your word has a length greater than 5.");
+	}
+}
+
+cond_output("Papaya")
+cond_output("Hello")
