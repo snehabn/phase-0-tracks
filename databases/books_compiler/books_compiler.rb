45c53d6e12bf99875e8aff63922a94bf75625952
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-04-25 00:40:00 -0700

    Finished writing main code for inserting and updating book information into the log.

--- databases/books_compiler/books_compiler.rb
+++ databases/books_compiler/books_compiler.rb
@@ -7,40 +7,31 @@
 #require gems
 
 require 'sqlite3'
-require 'faker'
+
 
 # create a SQLite 3 database to log books
 
 db = SQLite3::Database.new("books.db")
-rating = SQLite3::Database.new("rating.db")
 
 #save results within databases to a hash
 db.results_as_hash = true
-rating.results_as_hash = true
+
 
 #create a table within the db
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS books(
     id INTEGER PRIMARY KEY,
     book_name VARCHAR(255),
-    year_pubished INT,
+    year_pubished DATE,
     genre VARCHAR(255),
     author VARCHAR(255),
-    read VARCHAR(225),
-    rating_id INT,
-    FOREIGN KEY (rating_id) REFERENCES rating(id)
+    read VARCHAR(255),
+    rating REAL
   )
 SQL
 
-create_rating_cmd = <<-SQL
-	CREATE TABLE IF NOT EXISTS rating(
-		id INTEGER PRIMARY KEY,
-		rating VARCHAR(255)
-	) 
-SQL
 #create a table for books if it doesn't exist.
 db.execute(create_table_cmd)
-rating.execute(create_rating_cmd)
 
 # method that will add book to database
 def add_entry(db, book_name, year_pubished, genre, author, rating, read)
@@ -84,20 +75,26 @@ end
 # method that will display current contents of books database
 def display_entries(db)
 	entries = db.execute("SELECT * FROM books")
-	entries.each do |book|
- 		puts "Entry #{book['id']} is #{book['book_name']}. It was written by #{book['author']}, in the year #{book['year_pubished']}. Reading status: #{book['read']}."
- 		puts "This book belongs to the #{book['genre']} genre."
- 		puts "You have rated it a #{book['rating']} out 10."
- 		puts
+	if entries == []
+		puts "You have zero logged books!"
+		puts
+	else
+		entries.each do |book|
+	 		puts "#{book['id']}. #{book['book_name']} by #{book['author']}"
+	 		puts "	Published: #{book['year_pubished']}, Reading status: #{book['read']}"
+	 		puts " 	Genre: #{book['genre']}"
+	 		puts "	Your rating: #{book['rating']} out 10."
+	 		puts
+		end
 	end
 end
 
 # Driver codes to test the methods:
 
 # db.execute("INSERT INTO books (book_name, year_pubished, genre, author, rating, read)
-# 		 VALUES (?, ?, ?, ?, ?, ?)", ["Jungle Book", 1897, "Adventure", "Rudyard Kipling", 8, "currently reading"])
+# 		 VALUES (?, ?, ?, ?, ?, ?)", ["Jungle Book", 1897, "Adventure", "Rudyard Kipling", 8, "reading"])
 
-# add_entry(db, "Great Expectations", 1899, "Drama", "Charles Dickens", 4, "finished reading")
+# add_entry(db, "Great Expectations", 1899, "Drama", "Charles Dickens", 4, "not yet read")
 
 
 # update_rating_entry(db, 9, 2)
@@ -107,7 +104,143 @@ end
 # update_book_name_entry(db, "Peter Pan", 1)
 # display_entries(db)
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The user should be able to insert new book entries, update existing entries, and view entries by genre.
+
+# ask user how many books they would like to log
+puts "Hello and welcome to your book logging app!"
+puts
+
+	loop do 
+
+		puts "Please select from the following options:"
+		puts "1 - Enter a book into your log."
+		puts "2 - Update a current entry."
+		puts "3 - View your current log of books."
+		puts "4 - Exit the program."
+		user_num_entry = gets.chomp.to_i
+		puts
+
+		case user_num_entry
+
+		when 1
+			puts "How many books would you like to log in today?"
+			num_books = gets.chomp.to_i
+
+				i = 1
+				num_books.times do
+
+					puts "1. Please enter the title of book #{i}:"
+					book_name = gets.chomp
+					book_name = book_name.split.map(&:capitalize).join(' ')
+					puts
+
+					puts "2. What year was it published? If you do not know the year, please leave blank."
+					year_pubished = gets.chomp.to_i
+					puts
+
+					puts "3. What is the genre of the book:"
+					genre = gets.chomp.capitalize
+					puts
+
+					puts "4. Who is the author?"
+					author = gets.chomp.capitalize
+					author = author.split.map(&:capitalize).join(' ')
+					puts
+
+					puts "5. On a scale of 1 to 5 how would you rate this book?"
+					rating = gets.chomp.to_i
+					puts
+
+					puts "6. How far along are you in the book?"
+					read = gets.chomp.capitalize
+					puts
+
+					add_entry(db, book_name, year_pubished, genre, author, rating, read)
+
+					i += 1
+					puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+
+				end
+				display_entries(db)
+
+		when 2
+			puts "Here is your current log:"
+			display_entries(db)
+			puts
+			puts "Please select the number of the book you would like to update."
+			book_id = gets.chomp.to_i
+			puts
+			puts "What field would you like to update?"
+			puts "1 - Book Name"
+			puts "2 - Published Year"
+			puts "3 - Genre"
+			puts "4 - Author Name"
+			puts "5 - Your Rating"
+			update_num = gets.chomp.to_i
+
+			case update_num
+
+			when 1
+				puts "Please enter new book title:"
+				book_name = gets.chomp
+				book_name = book_name.split.map(&:capitalize).join(' ')
+				id = book_id
+				update_book_name_entry(db, book_name, id)
+				puts "The revised book name is #{book_name}."
+
+			when 2
+				puts "Please enter new published year:"
+				year_pubished = gets.chomp.to_i
+				update_year_published_entry(db, year_pubished, id)
+				puts "The revised published year is #{year_pubished}."
+
+			when 3
+				puts "Please enter new genre for the book:"
+				genre = gets.chomp.capitalize
+				update_genre_entry(db, genre, id)
+				puts "The revised genre is #{genre}."
+
+			when 4
+				puts "Please enter revised author's name:"
+				author = gets.chomp
+				author = author.split.map(&:capitalize).join(' ')
+				update_author_entry(db, author, id)
+				puts "The revised author name is #{author}."
+
+			when 5
+				puts "On a scale of 1 to 5, what would you like to change your rating to?"
+				rating = gets.chomp.to_f
+				update_rating_entry(db, rating, id)
+				puts "Your revised rating is #{rating}."
+
+			else
+				puts "You've selection is invalid."
+
+			end
+
+		when 3
+			display_entries(db)
+			
+
+		when 4
+			puts
+			puts "Thank you for logging your books!"
+			puts "Happy reading!!"
+			break
+		else
+			puts "You've entered an invalid selection, please try again."
+		end
+
+	end
+
+
+
+	
+
+			
+
 
 
 
