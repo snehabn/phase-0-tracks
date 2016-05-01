ecbd7e9e6ae62ba5e26b4f0c8e6270a9cdd91f4f
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-05-01 10:35:58 -0700

    Added jquery function to manipulate DOM in webpage.

---
+++ web_dev/jquery/learnjQuery.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+$('h1').css('border', '4px solid blue');
+
+$('img').delay(2000).slideUp(900).fadeIn(800);
+
+$('.photos').delay(6000).fadeOut(2000);
+
+$('.about').css('border', '4px solid black').delay(2000).fadeOut(900);
+
+$('div:last-child').css('color', '#C80000 ');
+
+$('button').css('font-family', 'cursive');
+
+});
